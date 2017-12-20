c930f931e15de73389230b76abb014398e95d7b5
Said <saidmlx@gmail.com>
2017-12-20 00:05:09 -0600

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Platzisquare';
+  places:any=[
+    {name:'Negocio 1' ,active:true},
+    {name:'Negocio 2' ,active:true},
+    {name:'Negocio 3' ,active:false},
+    {name:'Negocio 4' ,active:true},
+
+  ] 
+  lat:number=19.47853
+  lng:number=-99.0529936
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDVHSplyGEUhUuw4rZkvai-oXRxm5_ED6g'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
